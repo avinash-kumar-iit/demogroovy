c16463ce5767ee261a9ab4be92fa8584d494b457
avinash-kumar-iit <avinashkr.cseiitbhu@gmail.com>
2023-04-25 16:03:21 +0530

    update

---
+++ src/com/hdfc/demo.groovy
@@ -0,0 +1,19 @@
+package com.hdfc
+
+class Demo {
+    static void main(args) {
+        println 'Welcome to Javatpoint tutorial on Groovy...'
+
+        String s1 = "Javatpoint"
+        println "This is tutorial on Groovy at ${s1} "
+        String s4 = '''This is line 1  
+This is line 2  
+This is line 3  
+This is line 4  
+This is line 5'''
+        println s4
+        String s5 = /This is groovy tutorial and we are learning string/
+        println s5
+
+    }
+}
